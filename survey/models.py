b90419e51c78bd329136be52b25ecd686fb144fc
Alexis Bellido <alexis@ventanazul.com>
2012-06-18 22:24:23 +0000

    add winning_answers method

--- survey/models.py
+++ survey/models.py
@@ -1,20 +1,30 @@
 import datetime
 from django.db import models
+from django.db.models import Max
 
 class Survey(models.Model):
     title = models.CharField(max_length=60)
     opens = models.DateField()
-    closes = models.DateField()
+    closes = models.DateField(blank=True)
 
     def save(self, **kwargs):
         if not self.pk and self.opens and not self.closes:
-            #self.closes = self.opens + datetime.timedelta(7)
-            self.closes = self.opens
+            self.closes = self.opens + datetime.timedelta(7)
         super(Survey, self).save(**kwargs)
+
+    def __unicode__(self):
+        return '%s' % self.title
 
 class Question(models.Model):
     question = models.CharField(max_length=200)
     survey = models.ForeignKey(Survey)
+
+    def winning_answers(self):
+        rv = []
+        max_votes = self.answer_set.aggregate(Max('votes')).values()[0]
+        if max_votes and max_votes > 0:
+            rv = self.answer_set.filter(votes=max_votes)
+        return rv
 
     def __unicode__(self):
         return u'%s: %s' % (self.survey, self.question)
