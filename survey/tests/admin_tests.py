625fac2e54da991119211744b24acb72a7932b2f
Alexis Bellido <alexis@ventanazul.com>
2012-06-24 18:25:21 +0000

    use twill to talk directly to Django via WSGI, add reverse_for_twill

--- survey/tests/admin_tests.py
+++ survey/tests/admin_tests.py
@@ -2,6 +2,13 @@ import datetime
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.core.handlers.wsgi import WSGIHandler
+import twill
+TWILL_TEST_HOST = 'twilltest'
+twill.add_wsgi_intercept(TWILL_TEST_HOST, 80, WSGIHandler)
+
+def reverse_for_twill(named_url):
+    return 'http://' + TWILL_TEST_HOST + reverse(named_url)
 
 class AdminSurveyTest(TestCase):
     def setUp(self):
@@ -20,6 +27,7 @@ class AdminSurveyTest(TestCase):
     #    self.assertEqual(1, 1)
 
     def testAddSurveyError(self):
+        print reverse_for_twill('admin:survey_survey_add')
         post_data = {
             'title': u'Time Traveling',
             'opens': datetime.date.today(),
