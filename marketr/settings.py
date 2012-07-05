a731781cefc196c50bd7fb6e8899ca031588b349
Alexis Bellido <alexis@ventanazul.com>
2012-07-05 19:36:42 +0000

    add django-debug-toolbar default configuration

--- marketr/settings.py
+++ marketr/settings.py
@@ -97,6 +97,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -126,6 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'survey',
     'gen_utils',
+    'debug_toolbar',
 )
 
 # A sample logging configuration. The only tangible logging
